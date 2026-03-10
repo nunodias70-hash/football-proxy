4163dd8c1facaacbf53428dd80b0334035e86b88
nunodias70-hash <nunodias70@gmail.com>
2026-03-10 11:53:53 +0000

    index.js

--- index.js
+++ index.js
@@ -10,14 +10,8 @@ app.use((req, res, next) => {
 app.get('/api', async (req, res) => {
   const url = req.query.url;
   if (!url) return res.status(400).json({ error: 'Missing url' });
-  
   try {
-    const response = await fetch(url, {
-      headers: {
-        'X-RapidAPI-Key': '504070e6f7mshdeb6e962eff9fccp1e71b6jsn2eb2b7a7e9c7',
-        'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-      }
-    });
+    const response = await fetch(url);
     const data = await response.json();
     res.json(data);
   } catch (e) {
